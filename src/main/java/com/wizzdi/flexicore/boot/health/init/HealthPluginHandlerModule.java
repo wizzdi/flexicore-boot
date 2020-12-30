08d528e99a600b547cf94e256fc67ff34d8470d1
asaf <asaf@flexi-core.com>
2020-12-31 01:14:13 +0200

    init

---
+++ src/main/java/com/wizzdi/flexicore/boot/health/init/HealthPluginHandlerModule.java
@@ -0,0 +1,12 @@
+package com.wizzdi.flexicore.boot.health.init;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.wizzdi.flexicore.boot.health")
+public class HealthPluginHandlerModule {
+
+
+
+}
