2876cc2f207559f35cf3e95ee42d4509c1c8f893
asaf <asaf@flexi-core.com>
2020-12-31 15:50:59 +0200

    added testing

--- src/test/java/com/wizzdi/flexicore/boot/base/PluginLoadingTest.java
+++ src/test/java/com/wizzdi/flexicore/boot/base/PluginLoadingTest.java
@@ -2,15 +2,22 @@ package com.wizzdi.flexicore.boot.base;
 
 import com.wizzdi.flexicore.boot.base.app.App;
 import com.wizzdi.flexicore.boot.base.init.FlexiCorePluginManager;
+import com.wizzdi.flexicore.boot.test.helper.PluginJar;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,17 +25,32 @@ import java.util.stream.Collectors;
 @SpringBootTest(classes = App.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
+
 public class PluginLoadingTest {
 
-    @Autowired
-    private FlexiCorePluginManager flexiCorePluginManager;
 
-    @Test
-    public void testNoFailedPlugins(){
-        Set<String> started = flexiCorePluginManager.getStartedPlugins().stream().map(f -> f.getPluginId()).collect(Collectors.toSet());
-        Set<String> resolved = flexiCorePluginManager.getResolvedPlugins().stream().map(f -> f.getPluginId()).collect(Collectors.toSet());
-        Assert.assertEquals(started,resolved);
+	@Value("${flexicore.plugins}")
+	private String pluginsDir;
+
+	@Autowired
+	private FlexiCorePluginManager flexiCorePluginManager;
+
+    @BeforeAll
+    private void init() throws IOException {
+        PluginJar pluginZip = new PluginJar.Builder(new File(pluginsDir).toPath().resolve("my-plugin-1.2.3.zip"), "myPlugin")
+                .extension("com.wizzdi.flexicore.boot.base.pluginA.PluginAService")
+                .pluginVersion("1.2.3")
+                .build();
 
     }
+
+	@Test
+	public void testNoFailedPlugins() {
+
+		Set<String> started = flexiCorePluginManager.getStartedPlugins().stream().map(f -> f.getPluginId()).collect(Collectors.toSet());
+		Set<String> resolved = flexiCorePluginManager.getResolvedPlugins().stream().map(f -> f.getPluginId()).collect(Collectors.toSet());
+		Assert.assertEquals(started, resolved);
+
+	}
 
 }
---
+++ src/test/java/com/wizzdi/flexicore/boot/base/pluginA/PluginAService.java
@@ -0,0 +1,11 @@
+package com.wizzdi.flexicore.boot.base.pluginA;
+
+import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
+import org.pf4j.Extension;
+import org.springframework.stereotype.Component;
+
+@Extension
+@Component
+public class PluginAService implements Plugin {
+
+}
