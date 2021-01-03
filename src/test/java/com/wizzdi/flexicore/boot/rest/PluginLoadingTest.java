71abbd65ac5ba362ff4a62a8b622fdbbec6efe40
asaf <asaf@flexi-core.com>
2021-01-03 10:36:19 +0200

    added testing

--- src/test/java/com/wizzdi/flexicore/boot/rest/PluginLoadingTest.java
+++ src/test/java/com/wizzdi/flexicore/boot/rest/PluginLoadingTest.java
@@ -17,10 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -32,6 +35,9 @@ import java.util.stream.Collectors;
 public class PluginLoadingTest {
 
 	private static final Logger logger= LoggerFactory.getLogger(PluginLoadingTest.class);
+	private static final String pluginsPath;
+	private static final String entitiesPath;
+	private static final String PLUGIN_ID = "myPlugin";
 
 	@Value("${flexicore.plugins}")
 	private String pluginsDir;
@@ -42,17 +48,48 @@ public class PluginLoadingTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-    @BeforeAll
-    private void init() throws IOException {
-        PluginJar pluginZip = new PluginJar.Builder(new File(pluginsDir).toPath().resolve("my-plugin-1.2.3.zip"), "myPlugin")
-                .extension("com.wizzdi.flexicore.boot.rest.pluginA.PluginAService")
-                .pluginVersion("1.2.3")
-                .build();
+	static{
+		pluginsPath=getPluginsDir("plugins");
+		entitiesPath=getPluginsDir("entities");;
+		try {
+			File pluginsDir = new File(pluginsPath);
+			if (!pluginsDir.exists()) {
+				if (!pluginsDir.mkdirs()) {
+					logger.error("failed creating plugins dir");
+				}
+			}
+			PluginJar pluginZip = new PluginJar.Builder(pluginsDir.toPath().resolve("my-plugin-1.2.3.zip"), PLUGIN_ID)
+					.extension("com.wizzdi.flexicore.boot.rest.pluginA.PluginAService")
+					.pluginVersion("1.2.3")
+					.build();
+		}
+		catch (Exception e){
+			throw new RuntimeException(e);
+		}
 
-    }
+	}
 
+	@DynamicPropertySource
+	static void dynamicProperties(DynamicPropertyRegistry registry) throws IOException {
+
+		registry.add("flexicore.plugins", ()->pluginsPath);
+		registry.add("flexicore.entities", ()->entitiesPath);
+
+	}
+
+	private static String getPluginsDir(String prefix) {
+		try {
+			return Files.createTempDirectory(prefix).toFile().getAbsolutePath();
+
+		}
+		catch (Exception e){
+			logger.error("failed getting "+prefix+" dir",e);
+			return null;
+		}
+
+	}
 	@Test
-	public void testNoFailedPlugins() {
+	public void testControllerPlugin() {
 		ResponseEntity<String> test = restTemplate.getForEntity("/test", String.class);
 		Assertions.assertEquals(200,test.getStatusCodeValue());
 		String body = test.getBody();
