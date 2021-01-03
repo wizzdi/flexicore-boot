ea2c89712b7856db2482b52ab07dfd6ad4a959d3
asaf <asaf@flexi-core.com>
2021-01-03 10:44:12 +0200

    added jenkins file

--- src/test/java/com/wizzdi/flexicore/boot/jaxrs/PluginLoadingTest.java
+++ src/test/java/com/wizzdi/flexicore/boot/jaxrs/PluginLoadingTest.java
@@ -4,7 +4,6 @@ import com.wizzdi.flexicore.boot.base.init.FlexiCorePluginManager;
 import com.wizzdi.flexicore.boot.jaxrs.app.App;
 import com.wizzdi.flexicore.boot.test.helper.PluginJar;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,10 +15,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @ExtendWith(SpringExtension.class)
 
@@ -31,6 +33,9 @@ public class PluginLoadingTest {
 
 	private static final Logger logger= LoggerFactory.getLogger(PluginLoadingTest.class);
 
+	private static final String pluginsPath;
+	private static final String entitiesPath;
+	private static final String PLUGIN_ID = "myPlugin";
 	@Value("${flexicore.plugins}")
 	private String pluginsDir;
 
@@ -40,17 +45,50 @@ public class PluginLoadingTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-    @BeforeAll
-    private void init() throws IOException {
-        PluginJar pluginZip = new PluginJar.Builder(new File(pluginsDir).toPath().resolve("my-plugin-1.2.3.zip"), "myPlugin")
-                .extension("com.wizzdi.flexicore.boot.jaxrs.pluginA.PluginAService")
-                .pluginVersion("1.2.3")
-                .build();
+	static{
+		pluginsPath=getPluginsDir("plugins");
+		entitiesPath=getPluginsDir("entities");;
+		try {
+			File pluginsDir = new File(pluginsPath);
+			if (!pluginsDir.exists()) {
+				if (!pluginsDir.mkdirs()) {
+					logger.error("failed creating plugins dir");
+				}
+			}
+			PluginJar pluginZip = new PluginJar.Builder(pluginsDir.toPath().resolve("my-plugin-1.2.3.zip"), PLUGIN_ID)
+					.extension("com.wizzdi.flexicore.boot.jaxrs.pluginA.PluginAService")
+					.pluginVersion("1.2.3")
+					.build();
+		}
+		catch (Exception e){
+			throw new RuntimeException(e);
+		}
 
-    }
+	}
+
+	@DynamicPropertySource
+	static void dynamicProperties(DynamicPropertyRegistry registry) throws IOException {
+
+		registry.add("flexicore.plugins", ()->pluginsPath);
+		registry.add("flexicore.entities", ()->entitiesPath);
+
+	}
+
+	private static String getPluginsDir(String prefix) {
+		try {
+			return Files.createTempDirectory(prefix).toFile().getAbsolutePath();
+
+		}
+		catch (Exception e){
+			logger.error("failed getting "+prefix+" dir",e);
+			return null;
+		}
+
+	}
+
 
 	@Test
-	public void testNoFailedPlugins() {
+	public void testJAXRSPlugins() {
 		ResponseEntity<String> test = restTemplate.getForEntity("/FlexiCore/rest/testEntity/test", String.class);
 		Assertions.assertEquals(200,test.getStatusCodeValue());
 		String body = test.getBody();
