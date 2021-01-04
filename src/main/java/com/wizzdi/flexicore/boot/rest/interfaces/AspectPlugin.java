eded03e2d84b3f55d7cdf4ae002cf977ee949bd7
asaf <asaf@flexi-core.com>
2021-01-04 11:35:19 +0200

    prepare for release

--- src/main/java/com/wizzdi/flexicore/boot/rest/interfaces/AspectPlugin.java
+++ src/main/java/com/wizzdi/flexicore/boot/rest/interfaces/AspectPlugin.java
@@ -2,5 +2,5 @@ package com.wizzdi.flexicore.boot.rest.interfaces;
 
 import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 
-public class AspectPlugin implements Plugin {
+public interface AspectPlugin extends Plugin {
 }
