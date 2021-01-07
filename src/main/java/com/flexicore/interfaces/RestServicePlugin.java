dccbf6a1b464b8c6f97964a1b56b12aced626e2a
asaf <asaf@flexi-core.com>
2021-01-07 16:10:17 +0200

    added consumes produces annotations

--- src/main/java/com/flexicore/interfaces/RestServicePlugin.java
+++ src/main/java/com/flexicore/interfaces/RestServicePlugin.java
@@ -1,4 +1,11 @@
 package com.flexicore.interfaces;
 
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface RestServicePlugin extends com.wizzdi.flexicore.boot.jaxrs.interfaces.RestServicePlugin {
 }
