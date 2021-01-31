7b5f6300cdbf968c6d4c25fd491044054dbc02e1
asaf <asaf@flexi-core.com>
2021-02-01 01:30:50 +0200

    1.0.1 - removed dynamic invokers model dependency

--- src/main/java/com/flexicore/model/Baseclass.java
+++ src/main/java/com/flexicore/model/Baseclass.java
@@ -10,19 +10,19 @@ import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.annotation.JsonTypeResolver;
 import com.flexicore.annotations.AnnotatedClazz;
 import com.flexicore.data.jsoncontainers.FCTypeResolver;
-import com.wizzdi.flexicore.boot.rest.views.Views;
-import com.wizzdi.flexicore.boot.rest.views.Views.Full;
 import com.flexicore.interfaces.Syncable;
 import com.flexicore.security.SecurityContextBase;
+import com.wizzdi.flexicore.boot.rest.views.Views;
+import com.wizzdi.flexicore.boot.rest.views.Views.Full;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
-
 
 import javax.persistence.*;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -34,294 +34,295 @@ import java.util.concurrent.ConcurrentHashMap;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "json-type", visible = true)
 @JsonTypeResolver(FCTypeResolver.class)
 @Table(name = "baseclass", indexes = {@Index(name = "baseclass_id", columnList = "id", unique = true),
-        @Index(name = "clazz_idx", columnList = "clazz_id"),
-        @Index(name = "creator_idx", columnList = "creator_id"),
-        @Index(name = "name_idx", columnList = "name"),
-        @Index(name = "sort_idx", columnList = "name,id"),
-        @Index(name = "search_idx", columnList = "searchKey,dtype")
+		@Index(name = "clazz_idx", columnList = "clazz_id"),
+		@Index(name = "creator_idx", columnList = "creator_id"),
+		@Index(name = "name_idx", columnList = "name"),
+		@Index(name = "sort_idx", columnList = "name,id"),
+		@Index(name = "search_idx", columnList = "searchKey,dtype")
 })
 public class Baseclass implements Syncable {
 
-    private static ConcurrentHashMap<String, Clazz> allclazzes = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<String, Clazz> allclazzes = new ConcurrentHashMap<>();
 
 
-    @Lob
-    @JsonIgnore
-    private String searchKey;
-    private boolean softDelete;
+	@Lob
+	@JsonIgnore
+	private String searchKey;
+	private boolean softDelete;
 
 
-    @Column(name = "dtype", insertable = false, updatable = false)
-    private String dtype;
+	@Column(name = "dtype", insertable = false, updatable = false)
+	private String dtype;
 
-    @Id
-    protected String id;
-    protected String name;
-    protected String description;
+	@Id
+	protected String id;
+	protected String name;
+	protected String description;
 
-    @ManyToOne(targetEntity = SecurityUser.class)
-    @JsonView(Views.ForSwaggerOnly.class)
-    protected SecurityUser creator;
+	@ManyToOne(targetEntity = SecurityUser.class)
+	@JsonView(Views.ForSwaggerOnly.class)
+	protected SecurityUser creator;
 
-    @ManyToOne(targetEntity = SecurityTenant.class)
+	@ManyToOne(targetEntity = SecurityTenant.class)
 
-    private SecurityTenant tenant;
+	private SecurityTenant tenant;
 
-    private boolean systemObject;
+	private boolean systemObject;
 
-    @ManyToOne(targetEntity = SecurityTenant.class)
+	@ManyToOne(targetEntity = SecurityTenant.class)
 
-    public SecurityTenant getTenant() {
-        return tenant;
-    }
+	public SecurityTenant getTenant() {
+		return tenant;
+	}
 
-    public void setTenant(SecurityTenant tenant) {
-        this.tenant = tenant;
-    }
+	public void setTenant(SecurityTenant tenant) {
+		this.tenant = tenant;
+	}
 
-    @ManyToOne(targetEntity = Clazz.class)
-    @JsonView(Full.class)
-    protected Clazz clazz;
-    // this field is added to to trigger metadatamodel generation , sometimes _.clazz field isn't created.
+	@ManyToOne(targetEntity = Clazz.class)
+	@JsonView(Full.class)
+	protected Clazz clazz;
+	// this field is added to to trigger metadatamodel generation , sometimes _.clazz field isn't created.
 
 
-    @Column(columnDefinition = "timestamp with time zone")
-    private OffsetDateTime creationDate;
-    @Column(columnDefinition = "timestamp with time zone")
-    private OffsetDateTime updateDate;
+	@Column(columnDefinition = "timestamp with time zone")
+	private OffsetDateTime creationDate;
+	@Column(columnDefinition = "timestamp with time zone")
+	private OffsetDateTime updateDate;
 
-    @JsonIgnore
-    @OneToMany(targetEntity = PermissionGroupToBaseclass.class, mappedBy = "rightside")
-    private List<PermissionGroupToBaseclass> permissionGroupToBaseclasses = new ArrayList<>();
+	@JsonIgnore
+	@OneToMany(targetEntity = PermissionGroupToBaseclass.class, mappedBy = "rightside")
+	private List<PermissionGroupToBaseclass> permissionGroupToBaseclasses = new ArrayList<>();
 
 
-    public boolean isSoftDelete() {
-        return softDelete;
-    }
+	public boolean isSoftDelete() {
+		return softDelete;
+	}
 
-    public void setSoftDelete(boolean softDelete) {
-        this.softDelete = softDelete;
-    }
+	public void setSoftDelete(boolean softDelete) {
+		this.softDelete = softDelete;
+	}
 
 
-    public OffsetDateTime getCreationDate() {
-        return creationDate;
-    }
+	public OffsetDateTime getCreationDate() {
+		return creationDate;
+	}
 
 
-    public void setCreationDate(OffsetDateTime timestampField) {
-        this.creationDate = timestampField;
-    }
+	public void setCreationDate(OffsetDateTime timestampField) {
+		this.creationDate = timestampField;
+	}
 
 
-    public void init() {
-        if (id == null) {
-            id = getBase64ID();
-        }
+	public void init() {
+		if (id == null) {
+			id = getBase64ID();
+		}
 
-        if (creationDate == null) {
-            creationDate = OffsetDateTime.now();
-        }
+		if (creationDate == null) {
+			creationDate = OffsetDateTime.now();
+		}
 
 
-        Class<? extends Baseclass> javaClass = this.getClass();
-        String typeName = javaClass.getCanonicalName();
-        Clazz clazz = Clazz.class.equals(javaClass) && this.name.equals("com.flexicore.model.Clazz") ? (Clazz) this : allclazzes.get(typeName);
-        this.setClazz(clazz);
+		Class<? extends Baseclass> javaClass = this.getClass();
+		String typeName = javaClass.getCanonicalName();
+		Clazz clazz = Clazz.class.equals(javaClass) && this.name.equals("com.flexicore.model.Clazz") ? (Clazz) this : allclazzes.get(typeName);
+		this.setClazz(clazz);
 
-    }
+	}
 
 
-    public Baseclass() {
-        super();
+	public Baseclass() {
+		super();
 
-    }
+	}
 
-    public static String getBase64ID() {
-        String result;
-        try {
-            result = new String(Base64.encodeBase64(Hex.decodeHex(UUID.randomUUID().toString().replaceAll("-", "")
-                    .toCharArray())));
-            result = result.replace("/", "-"); // we cannot afford a slash
-            result = result.substring(0, 22); //we don't need the trailing ==
+	public static String getBase64ID() {
+		String result;
+		try {
+			result = new String(Base64.encodeBase64(Hex.decodeHex(UUID.randomUUID().toString().replaceAll("-", "")
+					.toCharArray())));
+			result = result.replace("/", "-"); // we cannot afford a slash
+			result = result.substring(0, 22); //we don't need the trailing ==
 
-        } catch (DecoderException e) {
-            result = "errorinid";
-        }
+		} catch (DecoderException e) {
+			result = "errorinid";
+		}
 
-        return result;
-    }
+		return result;
+	}
 
 
-    @Id
-    public String getId() {
+	@Id
+	public String getId() {
 
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    @JsonView(Full.class)
-    @ManyToOne(targetEntity = Clazz.class)
-    public Clazz getClazz() {
-        return clazz;
-    }
+	@JsonView(Full.class)
+	@ManyToOne(targetEntity = Clazz.class)
+	public Clazz getClazz() {
+		return clazz;
+	}
 
-    public void setClazz(Clazz clazz) {
-        this.clazz = clazz;
-    }
+	public void setClazz(Clazz clazz) {
+		this.clazz = clazz;
+	}
 
 
-    @JsonView(Views.ForSwaggerOnly.class)
-    @ManyToOne(targetEntity = SecurityUser.class)
-    public SecurityUser getCreator() {
-        return creator;
-    }
+	@JsonView(Views.ForSwaggerOnly.class)
+	@ManyToOne(targetEntity = SecurityUser.class)
+	public SecurityUser getCreator() {
+		return creator;
+	}
 
-    public void setCreator(SecurityUser creator) {
-        this.creator = creator;
-    }
+	public void setCreator(SecurityUser creator) {
+		this.creator = creator;
+	}
 
 
-    @JsonIgnore
-    @OneToMany(targetEntity = SecurityLink.class, mappedBy = "rightside")
-    public List<SecurityLink> securityLinks = new ArrayList<>();
+	@JsonIgnore
+	@OneToMany(targetEntity = SecurityLink.class, mappedBy = "rightside")
+	public List<SecurityLink> securityLinks = new ArrayList<>();
 
 
-    public Baseclass(String name, SecurityContextBase securityContext) {
-        this.name = name;
-        if (securityContext != null) {
-            this.creator = securityContext.getUser();
-            this.tenant = securityContext.getTenantToCreateIn();
-        }
+	public Baseclass(String name, SecurityContextBase securityContext) {
+		this.name = name;
+		if (securityContext != null) {
+			this.creator = securityContext.getUser();
+			this.tenant = securityContext.getTenantToCreateIn();
+		}
 
-        init();
+		init();
 
-    }
+	}
 
 
-    public static void addClazz(Clazz clazz) {
-        String key = clazz.getName();
-        if (!allclazzes.containsKey(key)) {
-            allclazzes.put(key, clazz);
-        }
-    }
+	public static void addClazz(Clazz clazz) {
+		String key = clazz.getName();
+		if (!allclazzes.containsKey(key)) {
+			allclazzes.put(key, clazz);
+		}
+	}
 
 
-    public static Clazz getClazzByName(String canonicalName) {
-        return allclazzes.get(canonicalName);
+	public static Clazz getClazzByName(String canonicalName) {
+		return allclazzes.get(canonicalName);
 
-    }
+	}
 
 
-    public OffsetDateTime getUpdateDate() {
-        return updateDate;
-    }
+	public OffsetDateTime getUpdateDate() {
+		return updateDate;
+	}
 
 
-    public void setUpdateDate(OffsetDateTime updateDate) {
-        this.updateDate = updateDate;
-    }
+	public void setUpdateDate(OffsetDateTime updateDate) {
+		this.updateDate = updateDate;
+	}
 
 
-    public static List<Clazz> getAllClazz() {
-        return new ArrayList<>(allclazzes.values());
-    }
+	public static List<Clazz> getAllClazz() {
+		return new ArrayList<>(allclazzes.values());
+	}
 
 
-    /**
-     * generates UUID from input string , for every run generateUUIDFromString(s)=generateUUIDFromString(s),
-     * id is cached to improve performance
-     *
-     * @param input
-     * @return generated/cached id
-     */
-    public static String generateUUIDFromString(String input) {
+	/**
+	 * generates UUID from input string , for every run generateUUIDFromString(s)=generateUUIDFromString(s),
+	 * id is cached to improve performance
+	 *
+	 * @param input
+	 * @return generated/cached id
+	 */
+	public static String generateUUIDFromString(String input) {
 
-        return UUID.nameUUIDFromBytes(input.getBytes()).toString()
-                .replaceAll("-", "")
-                .substring(0, 22);
+		return UUID.nameUUIDFromBytes(input.getBytes()).toString()
+				.replaceAll("-", "")
+				.substring(0, 22);
 
-    }
+	}
 
-    @JsonIgnore
-    @OneToMany(targetEntity = SecurityLink.class, mappedBy = "rightside")
-    public List<SecurityLink> getSecurityLinks() {
-        return securityLinks;
-    }
+	@JsonIgnore
+	@OneToMany(targetEntity = SecurityLink.class, mappedBy = "rightside")
+	public List<SecurityLink> getSecurityLinks() {
+		return securityLinks;
+	}
 
-    public void setSecurityLinks(List<SecurityLink> securityLinks) {
-        this.securityLinks = securityLinks;
-    }
+	public void setSecurityLinks(List<SecurityLink> securityLinks) {
+		this.securityLinks = securityLinks;
+	}
 
-    public boolean isSystemObject() {
-        return systemObject;
-    }
+	public boolean isSystemObject() {
+		return systemObject;
+	}
 
-    public Baseclass setSystemObject(boolean systemObject) {
-        this.systemObject = systemObject;
-        return this;
-    }
+	public Baseclass setSystemObject(boolean systemObject) {
+		this.systemObject = systemObject;
+		return this;
+	}
 
 
-    @Column(name = "dtype", insertable = false, updatable = false)
+	@Column(name = "dtype", insertable = false, updatable = false)
 
-    public String getDtype() {
-        return dtype;
-    }
+	public String getDtype() {
+		return dtype;
+	}
 
-    public <T extends Baseclass> T setDtype(String dtype) {
-        this.dtype = dtype;
-        return (T) this;
-    }
+	public <T extends Baseclass> T setDtype(String dtype) {
+		this.dtype = dtype;
+		return (T) this;
+	}
 
-    @Lob
-    @JsonIgnore
-    public String getSearchKey() {
-        return searchKey;
-    }
+	@Lob
+	@JsonIgnore
+	public String getSearchKey() {
+		return searchKey;
+	}
 
-    public <T extends Baseclass> T setSearchKey(String searchKey) {
-        this.searchKey = searchKey;
-        return (T) this;
-    }
+	public <T extends Baseclass> T setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
+		return (T) this;
+	}
 
-    @JsonIgnore
-    @OneToMany(targetEntity = PermissionGroupToBaseclass.class, mappedBy = "rightside")
-    public List<PermissionGroupToBaseclass> getPermissionGroupToBaseclasses() {
-        return permissionGroupToBaseclasses;
-    }
+	@JsonIgnore
+	@OneToMany(targetEntity = PermissionGroupToBaseclass.class, mappedBy = "rightside")
+	public List<PermissionGroupToBaseclass> getPermissionGroupToBaseclasses() {
+		return permissionGroupToBaseclasses;
+	}
 
-    public Baseclass setPermissionGroupToBaseclasses(List<PermissionGroupToBaseclass> permissionGroupToBaseclasses) {
-        this.permissionGroupToBaseclasses = permissionGroupToBaseclasses;
-        return this;
-    }
+	public Baseclass setPermissionGroupToBaseclasses(List<PermissionGroupToBaseclass> permissionGroupToBaseclasses) {
+		this.permissionGroupToBaseclasses = permissionGroupToBaseclasses;
+		return this;
+	}
 
-    @Override
-    @Transient
-    public boolean isNoSQL() {
-        return false;
-    }
+	@Override
+	@Transient
+	public boolean isNoSQL() {
+		return false;
+	}
+
 
 
 }
--- src/main/java/com/flexicore/model/SecurityUser.java
+++ src/main/java/com/flexicore/model/SecurityUser.java
@@ -6,11 +6,15 @@
  ******************************************************************************/
 package com.flexicore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.flexicore.security.SecurityContextBase;
 
 import javax.persistence.Entity;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 //the table name 'user' isn't allowed in Postgresql
 @SuppressWarnings("serial")
@@ -21,11 +25,40 @@ import javax.persistence.Table;
 
 public class SecurityUser extends SecurityEntity {
 
+	@JsonIgnore
+	@OneToMany(targetEntity = RoleToUser.class,mappedBy = "rightside")
+	private List<RoleToUser> roles=new ArrayList<>();
+
+	@JsonIgnore
+	@OneToMany(targetEntity = UserToBaseClass.class,mappedBy = "leftside")
+	private List<UserToBaseClass> userToBaseClasses=new ArrayList<>();
 
 	public SecurityUser() {
 	}
 
 	public SecurityUser(String name, SecurityContextBase securityContext) {
 		super(name, securityContext);
+	}
+
+	@JsonIgnore
+	@OneToMany(targetEntity = RoleToUser.class,mappedBy = "rightside")
+	public List<RoleToUser> getRoles() {
+		return roles;
+	}
+
+	public <T extends SecurityUser> T setRoles(List<RoleToUser> roles) {
+		this.roles = roles;
+		return (T) this;
+	}
+
+	@JsonIgnore
+	@OneToMany(targetEntity = UserToBaseClass.class,mappedBy = "leftside")
+	public List<UserToBaseClass> getUserToBaseClasses() {
+		return userToBaseClasses;
+	}
+
+	public <T extends SecurityUser> T setUserToBaseClasses(List<UserToBaseClass> userToBaseClasses) {
+		this.userToBaseClasses = userToBaseClasses;
+		return (T) this;
 	}
 }
