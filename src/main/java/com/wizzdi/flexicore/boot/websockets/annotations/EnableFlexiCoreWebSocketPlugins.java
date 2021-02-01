f4c398f979d343f6884e244c89b6eba83860d4a9
asaf <asaf@flexi-core.com>
2021-02-01 11:41:50 +0200

    added testing

--- src/main/java/com/wizzdi/flexicore/boot/websockets/annotations/EnableFlexiCoreWebSocketPlugins.java
+++ src/main/java/com/wizzdi/flexicore/boot/websockets/annotations/EnableFlexiCoreWebSocketPlugins.java
@@ -1,8 +1,11 @@
 package com.wizzdi.flexicore.boot.websockets.annotations;
 
 import com.wizzdi.flexicore.boot.websockets.init.WSPluginHandlerModule;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import java.lang.annotation.*;
 
@@ -11,5 +14,7 @@ import java.lang.annotation.*;
 @Documented
 @Import(WSPluginHandlerModule.class)
 @Configuration
+@AutoConfigureAfter(ServletServerContainerFactoryBean.class)
+@ConditionalOnWebApplication
 public @interface EnableFlexiCoreWebSocketPlugins {
 }
