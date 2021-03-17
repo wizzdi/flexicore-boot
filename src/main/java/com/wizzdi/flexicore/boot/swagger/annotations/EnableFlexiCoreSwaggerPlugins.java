d9a04c246864625db58b434fd217be592e7fba40
asaf <asaf@flexi-core.com>
2021-03-17 17:27:57 +0200

    init

---
+++ src/main/java/com/wizzdi/flexicore/boot/swagger/annotations/EnableFlexiCoreSwaggerPlugins.java
@@ -0,0 +1,15 @@
+package com.wizzdi.flexicore.boot.swagger.annotations;
+
+import com.wizzdi.flexicore.boot.swagger.init.SwaggerPluginHandlerModule;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+@Import(SwaggerPluginHandlerModule.class)
+@Configuration
+public @interface EnableFlexiCoreSwaggerPlugins {
+}
---
+++ src/main/java/com/wizzdi/flexicore/boot/swagger/service/SpringDocPluginConfiguration.java
@@ -0,0 +1,30 @@
+package com.wizzdi.flexicore.boot.swagger.service;
+
+import com.wizzdi.flexicore.boot.base.init.FlexiCorePluginManager;
+import org.springdoc.api.AbstractOpenApiResource;
+import org.springdoc.core.customizers.OpenApiBuilderCustomizer;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SpringDocPluginConfiguration {
+
+	@Bean
+	@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+	public OpenApiBuilderCustomizer openApiBuilderCustomizer(FlexiCorePluginManager pluginManager){
+		List<Map<String, Object>> restControllers = pluginManager.getStartedPlugins().stream().map(pluginManager::getApplicationContext).map(f -> f.getBeansWithAnnotation(RestController.class)).collect(Collectors.toList());
+		return openApiService -> {
+			restControllers.forEach(openApiService::addMappings);
+			restControllers.stream().map(Map::values).flatMap(Collection::stream).forEach(f->AbstractOpenApiResource.addRestControllers(ClassUtils.getUserClass(f.getClass())));
+		};
+	}
+}
