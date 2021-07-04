062405a02a54f5860f4a9146a353929bf138f25d
Asaf Ben Natan <asaf@wizzdi.com>
2021-07-04 23:24:00 +0300

    1.1.0 - updated dependencies

--- src/test/java/com/wizzdi/flexicore/boot/jaxrs/pluginA/PluginAService.java
+++ src/test/java/com/wizzdi/flexicore/boot/jaxrs/pluginA/PluginAService.java
@@ -3,19 +3,12 @@ package com.wizzdi.flexicore.boot.jaxrs.pluginA;
 import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 import org.pf4j.Extension;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/testEntity")
-@RequestScoped
 @Component
 @Extension
 public class PluginAService implements Plugin {
