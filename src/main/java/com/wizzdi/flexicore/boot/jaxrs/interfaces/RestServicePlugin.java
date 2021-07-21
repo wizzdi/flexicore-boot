df6fc422039b79056729bb450df7513675af7aca
Asaf Ben Natan <asaf@wizzdi.com>
2021-07-21 12:24:02 +0300

    added default json consume/produce

--- src/main/java/com/wizzdi/flexicore/boot/jaxrs/interfaces/RestServicePlugin.java
+++ src/main/java/com/wizzdi/flexicore/boot/jaxrs/interfaces/RestServicePlugin.java
@@ -2,5 +2,10 @@ package com.wizzdi.flexicore.boot.jaxrs.interfaces;
 
 import com.wizzdi.flexicore.boot.base.interfaces.Plugin;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+
+@Produces({"application/json"})
+@Consumes({"application/json"})
 public interface RestServicePlugin extends Plugin {
 }
