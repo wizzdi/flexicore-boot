10857d7e94511f653417a8fa624b59272d2cb138
Asaf Ben Natan <asaf@wizzdi.com>
2021-07-29 23:16:30 +0300

    added type retention

--- src/main/java/com/flexicore/annotations/TypeRetention.java
+++ src/main/java/com/flexicore/annotations/TypeRetention.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.METHOD})
+@Target({ElementType.FIELD,ElementType.METHOD,ElementType.ANNOTATION_TYPE})
 public @interface TypeRetention {
 
     Class<?> value() default Void.class;
