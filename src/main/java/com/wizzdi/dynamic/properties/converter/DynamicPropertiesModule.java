cf8723191b6045af1d4d8de5d1928e61eadf77b0
Asaf Ben Natan <asaf@wizzdi.com>
2021-10-17 18:24:46 +0300

    1.1.2

--- src/main/java/com/wizzdi/dynamic/properties/converter/DynamicPropertiesModule.java
+++ src/main/java/com/wizzdi/dynamic/properties/converter/DynamicPropertiesModule.java
@@ -3,8 +3,6 @@ package com.wizzdi.dynamic.properties.converter;
 import com.wizzdi.flexicore.boot.jpa.init.FlexiCoreJPAConfiguration;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
