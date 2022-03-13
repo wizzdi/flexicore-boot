1e6763ba9db80b3bc063b2dd4caef2bf898a89d6
Asaf Ben Natan <asaf@wizzdi.com>
2022-03-13 16:15:15 +0200

    1.1.1 - fixed mappings for regular swagger

--- src/main/java/com/wizzdi/flexicore/boot/swagger/service/SpringDocPluginConfiguration.java
+++ src/main/java/com/wizzdi/flexicore/boot/swagger/service/SpringDocPluginConfiguration.java
@@ -6,6 +6,7 @@ import org.springdoc.core.customizers.OpenApiBuilderCustomizer;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,10 @@ public class SpringDocPluginConfiguration {
 
 	@Bean
 	@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+	@Lazy
 	public OpenApiBuilderCustomizer openApiBuilderCustomizer(FlexiCorePluginManager pluginManager){
-		List<Map<String, Object>> restControllers = pluginManager.getStartedPlugins().stream().map(pluginManager::getApplicationContext).map(f -> f.getBeansWithAnnotation(RestController.class)).collect(Collectors.toList());
 		return openApiService -> {
+			List<Map<String, Object>> restControllers = pluginManager.getStartedPlugins().stream().map(pluginManager::getApplicationContext).map(f -> f.getBeansWithAnnotation(RestController.class)).collect(Collectors.toList());
 			restControllers.forEach(openApiService::addMappings);
 			restControllers.stream().map(Map::values).flatMap(Collection::stream).forEach(f->AbstractOpenApiResource.addRestControllers(ClassUtils.getUserClass(f.getClass())));
 		};
