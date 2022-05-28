c9c3a798b44664c0048037c49c5a00ce8d385781
Asaf Ben Natan <asaf@wizzdi.com>
2022-05-28 11:59:55 +0300

    2.0.0 - jdk17

--- src/test/java/com/wizzdi/flexicore/boot/jpa/eclipselink/PluginLoadingTest.java
+++ src/test/java/com/wizzdi/flexicore/boot/jpa/eclipselink/PluginLoadingTest.java
@@ -3,6 +3,7 @@ package com.wizzdi.flexicore.boot.jpa.eclipselink;
 import com.wizzdi.flexicore.boot.base.init.FlexiCorePluginManager;
 import com.wizzdi.flexicore.boot.jpa.eclipselink.app.App;
 import com.wizzdi.flexicore.boot.jpa.eclipselink.app.TestEntity;
+import com.wizzdi.flexicore.boot.jpa.eclipselink.pluginA.PluginARepository;
 import com.wizzdi.flexicore.boot.test.helper.PluginJar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -34,62 +35,15 @@ public class PluginLoadingTest {
 
 	private static final Logger logger= LoggerFactory.getLogger(PluginLoadingTest.class);
 
-	private static final String pluginsPath;
-	private static final String entitiesPath;
 	private static final String PLUGIN_ID = "myPlugin";
 	@Value("${flexicore.plugins}")
 	private String pluginsDir;
 
 	@Autowired
-	private FlexiCorePluginManager flexiCorePluginManager;
+	private PluginARepository pluginARepository;
 
 	@Autowired
 	private TestRestTemplate restTemplate;
-
-	static{
-		pluginsPath=getPluginsDir("plugins");
-		entitiesPath=getPluginsDir("entities");;
-		try {
-			File pluginsDir = new File(pluginsPath);
-			if (!pluginsDir.exists()) {
-				if (!pluginsDir.mkdirs()) {
-					logger.error("failed creating plugins dir");
-				}
-			}
-			PluginJar pluginZip = new PluginJar.Builder(pluginsDir.toPath().resolve("my-plugin-1.2.3.zip"), PLUGIN_ID)
-					.extension("com.wizzdi.flexicore.boot.jpa.eclipselink.pluginA.PluginAService")
-					.extension("com.wizzdi.flexicore.boot.jpa.eclipselink.pluginA.PluginARepository")
-					.extension("com.wizzdi.flexicore.boot.jpa.eclipselink.pluginA.PluginAController")
-					.extension("com.wizzdi.flexicore.boot.jpa.eclipselink.pluginA.Config")
-
-					.pluginVersion("1.2.3")
-					.build();
-		}
-		catch (Exception e){
-			throw new RuntimeException(e);
-		}
-
-	}
-
-	@DynamicPropertySource
-	static void dynamicProperties(DynamicPropertyRegistry registry) throws IOException {
-
-		registry.add("flexicore.plugins", ()->pluginsPath);
-		registry.add("flexicore.entities", ()->entitiesPath);
-
-	}
-
-	private static String getPluginsDir(String prefix) {
-		try {
-			return Files.createTempDirectory(prefix).toFile().getAbsolutePath();
-
-		}
-		catch (Exception e){
-			logger.error("failed getting "+prefix+" dir",e);
-			return null;
-		}
-
-	}
 
 
 	@Test
