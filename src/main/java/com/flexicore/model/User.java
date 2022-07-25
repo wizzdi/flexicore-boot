fb1e8f16aaafab34fa56a216b3d9509c81699143
Asaf Ben Natan <asaf@wizzdi.com>
2022-07-25 12:35:44 +0300

    1.1.1 , removed wildcrd import

--- src/main/java/com/flexicore/model/User.java
+++ src/main/java/com/flexicore/model/User.java
@@ -14,7 +14,13 @@ import com.flexicore.security.SecurityContextBase;
 import com.wizzdi.flexicore.boot.rest.views.Views;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
