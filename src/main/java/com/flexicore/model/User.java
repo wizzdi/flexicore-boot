9eb3c0bb7917c9bbc596e4c016839b8b4c5ec3f5
Asaf Ben Natan <asaf@wizzdi.com>
2022-08-24 16:25:01 +0300

    2.0.0 - jdk17

--- src/main/java/com/flexicore/model/User.java
+++ src/main/java/com/flexicore/model/User.java
@@ -24,6 +24,8 @@ import javax.persistence.Table;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import com.flexicore.model.SecurityUser;
+
 
 //the table name 'user' isn't allowed in Postgresql
 @SuppressWarnings("serial")
