92a73199b4d03c8c45dc917be7665bbd96876eff
Asaf Ben Natan <asaf@wizzdi.com>
2022-12-25 00:49:08 +0200

    3.0.0 - boot3

--- src/main/java/com/wizzdi/flexicore/common/user/request/CommonUserCreate.java
+++ src/main/java/com/wizzdi/flexicore/common/user/request/CommonUserCreate.java
@@ -7,9 +7,9 @@ import com.wizzdi.flexicore.security.validation.Create;
 import com.wizzdi.flexicore.security.validation.Update;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.OffsetDateTime;
 
 @Schema(description = "used to create a user")
